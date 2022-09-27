04a08b51f3cf8969b3aff8992d6df5b4498ef377
Fipeman <fsilvagalleguillos@gmail.com>
2022-09-26 21:43:12 -0300

    config tailwind & landing page approach
    setup config tailwind and add custom colors
    
    first approach of landing page, components created

---
+++ frontEnd/beermatch/pages/landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from '../src/components/login/login'
+
+function Landing() {
+  
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default Landing
