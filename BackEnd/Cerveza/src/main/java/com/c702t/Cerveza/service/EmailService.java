0d9963cb4eef3b3f6ec8807d8d154b79618b8ab1
AlexMoll2021 <alexmoll2008@hotmail.com>
2022-10-01 00:05:29 -0300

    add recoverPassWord and update pass

--- BackEnd/Cerveza/src/main/java/com/c702t/Cerveza/service/EmailService.java
+++ BackEnd/Cerveza/src/main/java/com/c702t/Cerveza/service/EmailService.java
@@ -8,4 +8,6 @@ public interface EmailService {
     void sendEmail(Mail email) throws IOException;
     void getEmailReady(String to, String template, String content, String subject) throws IOException;
     void checkFromRequest(String to, String from) throws IOException;
+    public void sendRecoverPassword(String to, String from, String passAux) throws IOException;
+
 }
