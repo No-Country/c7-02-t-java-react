0285132a400344f6ef83927a91cec796da093830
Jose Luis Panero Grando <57608370+junx2000@users.noreply.github.com>
2022-10-03 09:02:34 -0300

    Create slideEntity.java

---
+++ BackEnd/Cerveza/src/main/java/com/c702t/Cerveza/models/entity/slideEntity.java
@@ -0,0 +1,48 @@
+package com.c702t.Cerveza.models.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "slide")
+public class slideEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotEmpty(message = "The urlImage can't be null")
+    @NotBlank(message = "The urlImage can't be blank")
+    @Column( name = "url_image", nullable = false)
+    private String urlImage;
+
+    @Column(name = "timestamp", nullable = false)
+    @CreationTimestamp
+    private Timestamp timestamp;
+
+    /* TODO uncomment when BusinessEntity is created
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "business_id", nullable = false)
+    private BusinessEntity business;
+
+    Move this lines to BusinessEntity -->
+    @OneToMany(mappedBy = "business", fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    private Set<newsEntity> slide;
+    */
+
+
+
+}
