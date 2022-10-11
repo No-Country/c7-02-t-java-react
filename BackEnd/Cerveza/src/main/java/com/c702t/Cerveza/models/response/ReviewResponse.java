5262031932605d40f21cd5a7707ff40fc942db21
Jose Luis Panero Grando <57608370+junx2000@users.noreply.github.com>
2022-10-10 22:39:47 -0300

    Create ReviewResponse.java

---
+++ BackEnd/Cerveza/src/main/java/com/c702t/Cerveza/models/response/ReviewResponse.java
@@ -0,0 +1,26 @@
+package com.c702t.Cerveza.models.response;
+
+import io.swagger.annotations.ApiModel;
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel(description = "Class representing a Review response.")
+public class ReviewResponse {
+
+    private Long id;
+    private Long userId;
+    private Long businessId;
+    private Long attentionRate;
+    private Long priceRate;
+    private Long qualityRate;
+    private Long placeRate;
+    private Float totalRate;
+    private String text;
+    private Timestamp timestamp;
+}
