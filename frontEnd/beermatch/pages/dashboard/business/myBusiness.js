de2ab26a87c269ec07f4eb2ea6cee17ea15a04db
Fipeman <fsilvagalleguillos@gmail.com>
2022-10-19 13:25:48 -0300

    protected routes basic
    protected routes basic

--- frontEnd/beermatch/pages/dashboard/business/myBusiness.js
+++ frontEnd/beermatch/pages/dashboard/business/myBusiness.js
@@ -5,14 +5,35 @@ import NewBusiness from "../../../src/components/business/newBusiness";
 import SearchBusiness from "../../../src/components/business/searchBusiness";
 
 function myBusiness() {
-  // const userRol = localStorage.getItem("rolUser");
-  axios;
+  const userRol = localStorage.getItem("rolUser");
+
+  const baseURL = `http://localhost:8080/business/`;
+
+  // axios
+  //   .get(baseURL, {
+  //     params: {
+  //       userID: userRol
+  //     }
+  //   })
+  //   .then((response) => {
+  //     console.log(response.data);
+  //     // toast.success("Usuario creado !", {
+  //     //   position: toast.POSITION.TOP_CENTER,
+  //     // });
+  //     // router.push("/landing");
+  //   })
+  //   .catch((error) => {
+  //     // toast.error("Error de registro, pruebe nuevamente", {
+  //     //   position: toast.POSITION.TOP_CENTER,
+  //     // });
+  //     // console.log(error.message);
+  //   });
 
   return (
     <>
       <div className="lg:grid lg:grid-cols-2">
         <SearchBusiness />
-        <NewBusiness/>
+        <NewBusiness />
       </div>
       <BusinessCard />
     </>
